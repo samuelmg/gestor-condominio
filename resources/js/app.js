674d539ebc93b9b35022233892bb5e54fcfcc4a4
Samuel Mercado <samuel.mg@protonmail.com>
2022-11-17 02:33:34 -0600

    Registro de contenido de x-data desde bundle

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,9 @@
 import './bootstrap';
+import initAlpine from './init-alpine.js'
 
 import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
 
+Alpine.data('initAlpine', initAlpine)
 Alpine.start();
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,57 +12,6 @@
 
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/windmill/css/tailwind.output.css', 'resources/windmill/js/focus-trap.js'])
-    
-    <script>
-      document.addEventListener('alpine:init', () => {
-          Alpine.data('initAlpine', () => ({
-            {{-- dark: getThemeFromLocalStorage(), --}}
-            dark: false,
-            toggleTheme() {
-              this.dark = !this.dark
-              {{-- setThemeToLocalStorage(this.dark) --}}
-            },
-            isSideMenuOpen: false,
-            toggleSideMenu() {
-              this.isSideMenuOpen = !this.isSideMenuOpen
-            },
-            closeSideMenu() {
-              this.isSideMenuOpen = false
-            },
-            isNotificationsMenuOpen: false,
-            toggleNotificationsMenu() {
-              this.isNotificationsMenuOpen = !this.isNotificationsMenuOpen
-            },
-            closeNotificationsMenu() {
-              this.isNotificationsMenuOpen = false
-            },
-            isProfileMenuOpen: false,
-            toggleProfileMenu() {
-              this.isProfileMenuOpen = !this.isProfileMenuOpen
-            },
-            closeProfileMenu() {
-              this.isProfileMenuOpen = false
-            },
-            isPagesMenuOpen: false,
-            togglePagesMenu() {
-              this.isPagesMenuOpen = !this.isPagesMenuOpen
-            },
-            // Modal
-            isModalOpen: false,
-            trapCleanup: null,
-            openModal() {
-              this.isModalOpen = true
-              this.trapCleanup = focusTrap(document.querySelector('#modal'))
-            },
-            closeModal() {
-              this.isModalOpen = false
-              this.trapCleanup()
-            },
-          }))
-      })
-  </script>
-
-    {{-- <script src="../assets/js/init-alpine.js"></script> --}}
 
     <!-- Styles -->
     @livewireStyles
