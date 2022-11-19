6f73392581292b745c989d2907e4b8dfbd0f1774
Samuel Mercado <samuel.mg@protonmail.com>
2022-11-19 01:55:38 -0600

    Modelo y migración para Condominio y Vivienda

---
+++ app/Models/Condominio.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Condominio extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'condominio',
+        'pais_id',
+        'estado_id',
+        'municipio_id',
+        'localidad',
+        'colonia',
+        'calle',
+        'numero',
+    ];
+
+    public function pais()
+    {
+        return $this->belongsTo(Pais::class);
+    }
+
+    public function estado()
+    {
+        return $this->belongsTo(Estado::class);
+    }
+
+    public function municipio()
+    {
+        return $this->belongsTo(Municipio::class);
+    }
+}
