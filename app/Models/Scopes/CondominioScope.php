028b4c57c39ca91c050d96cdf79fe02cd1c06c9b
Samuel Mercado <samuel.mg@protonmail.com>
2022-12-04 23:29:04 -0600

    Crea scope global CondominioScope
    - Crea scope global CondominioScope para filtrar registros por condominio_id
    - Implementa CondominioScope en Modelo Vivienda

---
+++ app/Models/Scopes/CondominioScope.php
@@ -0,0 +1,24 @@
+<?php
+ 
+namespace App\Models\Scopes;
+ 
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Scope;
+use Illuminate\Support\Facades\Auth;
+
+class CondominioScope implements Scope
+{
+    /**
+     * Apply the scope to a given Eloquent query builder.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $builder
+     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @return void
+     */
+    public function apply(Builder $builder, Model $model)
+    {
+        $condominio_id = Auth::user()->condominios()->value('id');
+        $builder->where('condominio_id', $condominio_id);
+    }
+}
--- app/Models/Vivienda.php
+++ app/Models/Vivienda.php
@@ -4,11 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Scopes\CondominioScope;
 
 class Vivienda extends Model
 {
     use HasFactory;
     protected $fillable = ['numero', 'estatus', 'notas'];
+
+    protected static function booted()
+    {
+        static::addGlobalScope(new CondominioScope);
+    }
 
     public function condominio()
     {
