a1bfcf34f043722b46306df6bb68261d2231c0e0
Samuel Mercado <samuel.mg@protonmail.com>
2022-12-23 19:12:39 -0600

    Agrega relación entre Condominio y Persona
    - Agrega columna condominio_id a tabla personas
    - Crea relaciones entre Condominio y Persona
    - Implementa scope global CondominioScope en Persona

--- app/Models/Persona.php
+++ app/Models/Persona.php
@@ -2,12 +2,23 @@
 
 namespace App\Models;
 
+use App\Models\Scopes\CondominioScope;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Persona extends Model
 {
     use HasFactory;
+
+    protected static function booted()
+    {
+        static::addGlobalScope(new CondominioScope);
+    }
+
+    public function condominio()
+    {
+        return $this->belongsTo(Condominio::class);
+    }
 
     public function viviendas()
     {
