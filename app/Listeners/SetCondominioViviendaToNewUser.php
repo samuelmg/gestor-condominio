98944dfa17de36599c92a3ff09ac900160bd85d6
Samuel Mercado <samuel.mg@protonmail.com>
2022-12-26 02:33:46 -0600

    Implementa registro de viviendas con usuario nuevo
    - Modifica consulta de persona para incluir relación a viviendas sin usar scope global
    - Crea relación entre un nuevo usuario y viviendas

--- app/Listeners/SetCondominioViviendaToNewUser.php
+++ app/Listeners/SetCondominioViviendaToNewUser.php
@@ -28,8 +28,13 @@ class SetCondominioViviendaToNewUser
      */
     public function handle(Registered $event)
     {
-        $persona = Persona::withoutGlobalScope(CondominioScope::class)->where('tel_celular', $event->user->cel_number)->first();
+        $persona = Persona::withoutGlobalScope(CondominioScope::class)
+            ->where('tel_celular', $event->user->cel_number)
+            ->with('viviendas', function($query) {
+                $query->withoutGlobalScope(CondominioScope::class);
+            })
+            ->first();
         $event->user->condominios()->attach($persona->condominio_id);
-        // $event->user->viviendas()->attach($persona->viviendas->pluck('id')->toArray());
+        $event->user->viviendas()->attach($persona->viviendas->pluck('id')->toArray());
     }
 }
