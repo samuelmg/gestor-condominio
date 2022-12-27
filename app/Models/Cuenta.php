4a76ec78605ee9c1eb6716f3e6b66c267d277b13
Samuel Mercado <samuel.mg@protonmail.com>
2022-12-27 00:24:28 -0600

    Implementa CodominioScope en Cuenta

--- app/Models/Cuenta.php
+++ app/Models/Cuenta.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\Scopes\CondominioScope;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -9,6 +10,11 @@ class Cuenta extends Model
 {
     use HasFactory;
     public $timestamps = false;
+
+    protected static function booted()
+    {
+        static::addGlobalScope(new CondominioScope);
+    }
 
     public function condominio()
     {
